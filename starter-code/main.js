18c2fc12c8fdd64d68894fc637105eb31da6edef
Lisa Louison <lisa.louison@gmail.com>
2017-03-22 15:31:17 -0400

    added matching conditional statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//creating matching conditional statements
+if(cardOne === cardTwo){
+	alert("You found a match!")
+}
+else if(cardThree === cardFour){
+	alert("You found a match!")
+}
+else alert("Sorry, try again");
