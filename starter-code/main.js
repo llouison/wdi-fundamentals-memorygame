4b3d6e0ae6fb8c5d0b2efd65cf281ff56570c850
Lisa Louison <lisa.louison@gmail.com>
2017-03-23 13:57:42 -0400

    created cards in main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,10 +7,22 @@ var cardThree = "king";
 var cardFour = "king";
 
 //creating matching conditional statements
-if(cardOne === cardTwo){
-	alert("You found a match!")
+// if(cardOne === cardTwo){
+// 	alert("You found a match!")
+// }
+// else if(cardThree === cardFour){
+// 	alert("You found a match!")
+// }
+// else alert("Sorry, try again");
+
+var gameBoard = document.getElementById("game-board");
+
+var createCards = function(){
+	for (i = 0; i < 4; i++){
+		var newCard = document.createElement('div')
+		newCard.className = 'card';
+		document.getElementById('game-board').appendChild(newCard);
+	}
 }
-else if(cardThree === cardFour){
-	alert("You found a match!")
-}
-else alert("Sorry, try again");
+
+createCards();
